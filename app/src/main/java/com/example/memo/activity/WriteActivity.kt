6714fea55685c7db2c1d1a59c4835d0701751c79
yang0925 <keun0602@naver.com>
2020-10-22 14:00:40 +0900

    [Update] update

--- app/src/main/java/com/example/memo/activity/WriteActivity.kt
+++ app/src/main/java/com/example/memo/activity/WriteActivity.kt
@@ -8,5 +8,7 @@ class WriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
+
+
     }
 }
